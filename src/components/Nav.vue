d15de1cff5a4de452a0a0ce0dc2a27b00d16e5f4
wen yo <wenda897436@gmail.com>
2022-08-17 17:56:55 +0800

    nav style

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,11 +1,57 @@
 <script setup>
-import SvgIcon from "./SvgIcon.vue";
+import NavItem from "./NavItem.vue";
+const NAV_ITEMS = [
+  {
+    icon: "home",
+    text: "Home",
+    route: () => {},
+  },
+  {
+    icon: "work",
+    text: "Experience",
+    route: () => {},
+  },
+  {
+    icon: "note",
+    text: "Note",
+    route: () => {},
+  },
+];
+
+const PROJECT_ITEMS = [
+  {
+    icon: "wellcome",
+    text: "Wellcome",
+    route: () => {},
+  },
+  {
+    icon: "yui-pai",
+    text: "悅排賽",
+    route: () => {},
+  },
+  {
+    icon: "ct",
+    text: "Label System",
+    route: () => {},
+  },
+  {
+    icon: "dna",
+    text: "GeneBox 後臺系統",
+    route: () => {},
+  },
+];
 </script>
 
 <template>
   <div class="h-screen w-1/4 max-w-[250px] bg-dark-700 px-2.5 py-5">
-    <div class="text-primary-100 px-[30px] py-5">Wen You</div>
+    <div class="text-white px-[30px] py-5 text-xl">Wen You</div>
+    <template v-for="({ icon, text, route }, key) in NAV_ITEMS" :key="key">
+      <NavItem :icon="icon" :text="text" :route="route" />
+    </template>
 
-    <SvgIcon class="w-4 h-4" name="home"></SvgIcon>
+    <div class="px-[30px] mt-4 mb-2 text-xs text-primary-100 opacity-50">Projects</div>
+    <template v-for="({ icon, text, route }, key) in PROJECT_ITEMS" :key="key">
+      <NavItem :icon="icon" :text="text" :route="route" />
+    </template>
   </div>
 </template>
---
+++ src/components/NavItem.vue
@@ -0,0 +1,20 @@
+<script setup>
+import SvgIcon from "./SvgIcon.vue";
+import { defineProps } from "vue";
+const props = defineProps({
+  icon: String,
+  text: String,
+  route: String,
+  active: Boolean,
+});
+</script>
+
+<template>
+  <div
+    class="px-[30px] py-1 flex items-center cursor-pointer duration-300 hover:bg-primary-300 hover:rounded"
+    :class="{ 'bg-primary-300 rounded': active }"
+  >
+    <SvgIcon class="w-4 h-4" :name="icon" color="#C2D2FB"></SvgIcon>
+    <span class="text-sm ml-2 text-primary-100">{{ text }}</span>
+  </div>
+</template>
