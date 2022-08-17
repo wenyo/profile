0185a641e8a1dffc1851ab9dfb15a8dbbe673441
wen yo <wenda897436@gmail.com>
2022-08-17 16:32:52 +0800

    add color

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,12 +1,11 @@
 <script setup>
 import SvgIcon from "./SvgIcon.vue";
-import Colors from '../util/color'
 </script>
 
 <template>
   <div class="h-screen w-1/4 max-w-[250px] bg-dark-700 px-2.5 py-5">
     <div class="text-primary-100 px-[30px] py-5">Wen You</div>
 
-    <SvgIcon class="text-white" name="home"></SvgIcon>
+    <SvgIcon class="w-4 h-4" name="home"></SvgIcon>
   </div>
 </template>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,23 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
+    colors:{
+      primary:{
+        100:"#C2D2FB",
+        200:"#6D96FF",
+        300:"#1A57F3",
+        400:"#042987",
+        500:"#0A0414"
+      },
+      dark:{
+        100:"#6A6A6B",
+        300:"#40414A",
+        500:"#282931",
+        700:"#1D1E24",
+        900:"#01021B",
+      },
+      white: "#fff"
+    },
     extend: {},
   },
   plugins: [],
